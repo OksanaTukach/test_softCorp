95548823ce9b23f7d1fbba9e7194c6913d5e4554
OksTukach <Okstukach@gmail.com>
2022-04-04 10:33:49 +0300

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,71 @@
-"use strict";$(document).ready(function(){var e=$(window).width(),t=$(window).height();console.log("Width: "+e+"px"),console.log("Height: "+t+"px"),headerMainScreen(),$("body").on("click",".header__burger",function(){$(this).toggleClass("header__burger_close"),$(this).siblings(".header__menu").toggleClass("header__menu_open"),n()});var n=function(){var e=document.querySelector(".header__menu"),t=document.querySelector(".header");setTimeout(function(){e.style.height=window.innerHeight-t.clientHeight+"px"},1)};!function(){function e(e){e=window.event||e;var t=Math.max(-1,Math.min(1,e.wheelDelta||-e.detail));document.querySelector(".order__points_scroll").scrollLeft-=10*t,e.preventDefault()}document.querySelector(".order__points_scroll").addEventListener?(document.querySelector(".order__points_scroll").addEventListener("mousewheel",e,!1),document.querySelector(".order__points_scroll").addEventListener("DOMMouseScroll",e,!1)):document.querySelector(".order__points_scroll").attachEvent("onmousewheel",e)}(),$(".slider").slider({range:"max",min:1,max:100,value:75,slide:function(e,t){$(".form__item_value .value").html(t.value+"%")}}),$(".form__item_value .value").html($(".slider").slider("value")+"%");new LazyLoad({elements_selector:".lazy",load_delay:100});$(".styler").length&&$(".styler").styler()}),$(window).resize(function(){$(window).width(),$(window).height();headerMainScreen()});var headerMainScreen=function(){var e=document.querySelector(".main-screen"),t=document.querySelector(".main-screen__information"),n=document.querySelector(".header");setTimeout(function(){e.style.height=t.clientHeight+n.clientHeight+"px"},100)};
+"use strict";
+
+$(document).ready(function () {
+  var width = $(window).width();
+  var height = $(window).height();
+  console.log('Width: ' + width + 'px');
+  console.log('Height: ' + height + 'px');
+  headerMainScreen();
+  $('body').on('click', '.header__burger', function () {
+    $(this).toggleClass('header__burger_close');
+    $(this).siblings('.header__menu').toggleClass('header__menu_open');
+    headerMobHeight();
+  });
+
+  var headerMobHeight = function headerMobHeight() {
+    var headerMob = document.querySelector('.header__menu');
+    var header = document.querySelector('.header');
+    setTimeout(function () {
+      headerMob.style.height = window.innerHeight - header.clientHeight + 'px';
+    }, 1);
+  };
+
+  (function () {
+    function scrollHorizontally(e) {
+      e = window.event || e;
+      var delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+      document.querySelector('.order__points_scroll').scrollLeft -= delta * 10;
+      e.preventDefault();
+    }
+
+    if (document.querySelector('.order__points_scroll').addEventListener) {
+      document.querySelector('.order__points_scroll').addEventListener("mousewheel", scrollHorizontally, false);
+      document.querySelector('.order__points_scroll').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+    } else {
+      document.querySelector('.order__points_scroll').attachEvent("onmousewheel", scrollHorizontally);
+    }
+  })();
+
+  $(".slider").slider({
+    range: "max",
+    min: 1,
+    max: 100,
+    value: 75,
+    slide: function slide(event, ui) {
+      $(".form__item_value .value").html(ui.value + '%');
+    }
+  });
+  $(".form__item_value .value").html($(".slider").slider("value") + '%');
+  var lazyLoadInstance = new LazyLoad({
+    elements_selector: ".lazy",
+    load_delay: 100
+  });
+
+  if ($('.styler').length) {
+    $('.styler').styler();
+  }
+});
+$(window).resize(function () {
+  var width = $(window).width();
+  var height = $(window).height();
+  headerMainScreen();
+});
+
+var headerMainScreen = function headerMainScreen() {
+  var mainScreen = document.querySelector('.main-screen');
+  var mainScreenText = document.querySelector('.main-screen__information');
+  var header = document.querySelector('.header');
+  setTimeout(function () {
+    mainScreen.style.height = mainScreenText.clientHeight + header.clientHeight + 'px';
+  }, 100);
+};
